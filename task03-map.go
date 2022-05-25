e181c6b04992bd8a2bc3c1e9de581eba07c5d217
Igor Yagodin <Igor.Yagodin@x5.ru>
2022-05-25 19:49:27 +0300

    5

--- task03-map.go
+++ task03-map.go
@@ -6,6 +6,10 @@ func sortMapValues(input map[int]string) (result []string) {
 
 	keys := make([]int, 0, len(input))
 
+	for k := range input {
+		keys = append(keys, k)
+	}
+
 	sort.Ints(keys)
 
 	result = make([]string, 0)
